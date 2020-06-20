95e6abbfed395b78f468b4855f43acb406079fac
Josh <59149060+j-o-sh@users.noreply.github.com>
2020-06-20 13:32:40 +0200

    add 500 response

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ export default function (callback) {
         statusCode: 405,
         body: `http method ${calledMethod} is not allowed here!`
       })
-    } 
+    },
+    serverError(error) {
+      callback(null, {
+        statusCode: 500,
+        body: `Internal server error: ${error}`
+      })
+    }
   }
 }
